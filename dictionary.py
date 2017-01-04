3b070e514139a5b73d686f95537ffe4adda85f63
samcoppini <samcoppini@users.noreply.github.com>
2017-01-03 17:55:50 -0800

    Rewrite make_word

--- dictionary.py
+++ dictionary.py
@@ -3,69 +3,52 @@ import io
 import os
 import random
 
+def randchance(n):
+	return random.randint(0, n - 1) == 0
+
 def percent_vowels(word):
-	if word.count('\'') == 0 or (word.count('\'') == 1 and random.random() > 0.6) or (word.count('\'') == 2 and random.random() > 0.95):
-		return (word.count('a')+word.count('i')+word.count('u')+word.count('o'))/float(len(word))
-	else:
-		return 0;
+	num_vowels = 0
+	for char in word:
+		if char in 'aeiou':
+			num_vowels += 1
+	return num_vowels / len(word)
 
 def make_word():
-	word = random.choice('aioubbbdffjnllkpppsttttttmaiobbbdffjnllkpppsttttttm')
-	while random.randint(0,14-len(word)) != 0:
-		if word[-1] == 'a':
-			word += random.choice('bndkpstuxxxx')
-		elif word[-1] == 'b':
-			if len(word) > 2 and word[-2] != 'b':
-				word += random.choice('aioub\'x')
+	letter_chances = {
+		" ": "aabbbbbddffffiijjkkllllmmnnoopppppsstttttttttttu",
+		"a": "bdknpstuxxxx",
+		"b": "abiou'x",
+		"d": "adiou'xx",
+		"f": "ailo'xx",
+		"i": "bdfiklnptxxxxx",
+		"j": "aio",
+		"k": "isux",
+		"l": "fikloptu",
+		"m": "aiou'",
+		"n": "adikstu",
+		"o": "bdfklnos'xxxx",
+		"p": "o'x",
+		"s": "ailnstu'x",
+		"t": "ailnstu'x",
+		"u": "bdnpxxx",
+		"'": "klnt"
+	}
+	word = ""
+	while not word:
+		word = random.choice(letter_chances[" "])
+
+		while not randchance(14 - len(word)) and word[-1] != 'x':
+			word += random.choice(letter_chances[word[-1]])
+
+		if 0.25 < percent_vowels(word) < 0.75 and len(word) > 3:
+			if word[-1] == "'":
+				word = word[:-1]
+			if random.random() > .99:
+				return word + '-' + make_word()
 			else:
-				word += random.choice('aiou\'x')
-		elif word[-1] == 'd':
-			if len(word) > 2 and word[-2] != 'd':
-				word += random.choice('aioud\'xx')
-			else:
-				word += random.choice('aiou\'xx')
-		elif word[-1] == 'f':
-			word += random.choice('aiol\'xx')
-		elif word[-1] == 'j':
-			word += random.choice('aio')
-		elif word[-1] == 'i':
-			word += random.choice('ibdfnlkptxxxxx')
-		elif word[-1] == 'n':
-			word += random.choice('aiudtks')
-		elif word[-1] == 'l':
-			if len(word) > 2 and word[-2] in 'aiou':
-				word += random.choice('ioulktfp')
-			else:
-				word += random.choice('ioux')
-		elif word[-1] == 'k':
-			word += random.choice('iusx')
-		elif word[-1] == 'p':
-			word += random.choice('o\'x')
-		elif word[-1] == 's':
-			if len(word) > 2 and word[-2] in 'aiou':
-				word += random.choice('tianslux\'')
-			else:
-				word += random.choice('tlia\'')
-		elif word[-1] == 't':
-			word += random.choice('aiouaiouaioutxxx')
-		elif word[-1] == 'o':
-			word += random.choice('fsnbdoklxxxx\'')
-		elif word[-1] == 'u':
-			word += random.choice('bpndxxx')
-		elif word[-1] == '\'':
-			word += random.choice('ntkl')
-		elif word[-1] == 'm':
-			word += random.choice('aiou\'')
-		if word[-1] == 'x':
-			word = word[:-1]
-			break
-	if percent_vowels(word) > 0.25 and percent_vowels(word) < 0.75 and len(word) > 3 and word[-1] != '\'':
-		if random.random() > .99:
-			return word + '-' + make_word()
+				return word
 		else:
-			return word
-	else:
-		return make_word()
+			word = ""
 
 def unique_word():
 	word = make_word()
